760fa5493dafac72e0c3a9bf4aa0f829f3c660eb
Thomas Rohde <trohde@rocketmail.com>
2021-01-30 16:15:12 -0600

    setting up AddMovie form. Added movie title input, movie url input, movie description, cancel & save buttons. started applying material ui, started writing handleAddMovie and sendHome functions

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,7 +1,65 @@
-
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { Box, Typography, FormControl, Input, InputLabel, OutlinedInput, TextField, Button, Divider } from '@material-ui/core';
+import { ArrowForward, FavoriteBorder } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
 
 function AddMovie() {
     console.log('in AddMovie')
-}
+
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const [movieTitle, setMovieTitle] = useState('');
+    const [movieImage, setMovieImage] = useState('');
+
+    const handleAddMovie = () => {
+        console.log('clicked handleAddMovie');
+        event.preventDefault();
+
+        setMovieTitle('');
+        setMovieImage('');
+        history.push('/');
+    }; //end handleAddMovie
+
+    const sendHome = () => {
+        history.push('/');
+    }
+
+
+
+
+
+    return (
+        <div>
+            <form onSubmit={handleAddMovie}>
+                <FormControl variant="outlined">
+                    <InputLabel htmlFor="component-outlined">movie title</InputLabel>
+                    <OutlinedInput id="component-outlined" type="text" value={movieTitle} onChange={(event) => setMovieTitle(event.target.value)} variant="outlined" />
+                </FormControl>
+                <FormControl variant="outlined">
+                    <InputLabel htmlFor="component-outlined">movie url</InputLabel>
+                    <OutlinedInput id="component-outlined" type="text" value={movieImage} onChange={(event) => setMovieImage(event.target.value)} variant="outlined" />
+                </FormControl>
+                <FormControl variant="outlined">
+                    <TextField id="outlined-basic" label="description"  variant="outlined" />
+                </FormControl>
+                <FormControl></FormControl>
+                <Button
+                variant="contained"
+                color="secondary"
+                onClick={sendHome}
+                >CANCEL</Button>
+                <Button
+                variant="contained"
+                color="primary"
+                onClick={handleAddMovie}
+                >SAVE</Button>
+            </form>
+        </div>
+
+    )
+
+}; //end AddMovie
 
 export default AddMovie;
